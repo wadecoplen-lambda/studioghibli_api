9e99f1031dac18299d0a29fc732b62bfa078c63a
Wade-Coplen <wadecoplen@gmail.com>
2020-08-11 10:46:31 -0700

    complete

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
-const MovieCard = ({title, director, releaseDate, desc}) => {
-    return <div className='film-list'>Placeholder</div>
+const MovieCard = ({title, description, director, releaseDate}) => {
+    return <div className='film-list'>
+        <p>Film Title: {title}</p>
+        <p>Description: {description}</p>
+        <p>Director: {director} </p>
+        <p>Release Date: {releaseDate}</p>
+    </div>
 }
 
 export default MovieCard;
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -2,26 +2,41 @@ import React, {useState, useEffect} from 'react'
 import axios from 'axios';
 import MovieCard from './MovieCard';
 
-function MovieList() {
-    //set up state for the film data we are going to pull from the API
-    const [films, setFilms]= useState([]) //set as an empty array by default. It is good practice. 
-    //useEffect is desgin for impure functions and is used to control unpredictable behavior. 
+export default function MovieList() {
+    const [films, setFilms]= useState([])
     useEffect(() => {
         axios
         .get('https://ghibliapi.herokuapp.com/films')
         .then(res => {
-            console.log('Success', res)
+            //Why do we do this? We set state to the data returned from the Promise. Therefore, as the data changes on the API, so does the state of that data.
+            setFilms(res.data)
+        console.log('Success', res)
         })
-        .catch(err => {
-            console.log('Error', err)
+        .catch(error => {
+            console.log('Error', error)
         })
-    })
+    }, [])
+    // add our dependency array to ensure the code runs only once and prevents a loop.
+    //a dependency array accepts the state and updates on a state change
+
     return (
         <div className= 'title'>
-            <MovieCard />
+           {films.map(item => {
+               //Right here was bringing in the MovieCard component and setting the 'props' that will be rendered in MovieCard
+               return <MovieCard 
+                key = {item.id}
+                title = {item.title}
+                description = {item.description}
+                director = {item.director}
+                releaseDate = {item.release_date}
+               
+               />
+           })
+
+           }
+            
+                
         </div>
     )
 }
-
-export default MovieList;
 
